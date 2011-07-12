bf01b92bffaaf176b1396bc23be99c4246ee87e1
Shane Preece <shamess@gmail.com>
2011-07-12 13:52:16 +0100

    Added plugin meta head data

---
+++ orphaned-media.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Orphaned Media Import
+Plugin URI:
+Description: For whatever reason, some times you end up with media in your uploaded content that isn't in the Media Library. This plugin hopes to fix that issue.
+Version: 0.01
+Author: Shamess
+Author URI: http://shamess.info
+License: GPL2
+*/
+
+
+
+?>
